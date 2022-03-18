07e448bb657c254d04e3c1dbb60bb511b3ca7dd3
Raze91 <hattab_sami@yahoo.fr>
2022-03-18 17:44:26 +0100

    add exercises 8 with exercises 5 and 6

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../components/exercice_7.dart';
+import '../components/list.dart';
+import '../carousel/index.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -12,8 +14,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int index = 0;
+
+  List<Widget> list = [
+    CarouselImages(),
+    const CustomList(),
+    const Exercice7(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Exercice7());
+    // return const Scaffold(body: Exercice7());
+    return Scaffold(
+      body: list[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (int i) {
+          setState(() {
+            index = i;
+          });
+        },
+        backgroundColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        iconSize: 48,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
+        ],
+      ),
+    );
   }
 }
